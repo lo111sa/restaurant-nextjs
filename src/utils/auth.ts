0d9fe7a9faac199d135695698f56a0a3f5bc3729
lo111sa <losaberidze777@yahoo.com>
2023-08-23 23:26:25 +0400

    Auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,17 +1,17 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { getServerSession } from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "./connect";
+import NextAuth from "next-auth/next";
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
+  //adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
+      clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
     // ...add more providers here
   ],
 };
-
-export const getAuthSession = () => getServerSession(authOptions);
+export default NextAuth(authOptions);
